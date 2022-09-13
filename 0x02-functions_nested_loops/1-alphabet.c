d42659e37c1b6bcdcbb95b6eddd5de7298032343
Atwau.Pius <atwaup7@gmail.com>
2022-09-13 12:57:34 -0700

    task 1 debugging

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,12 +7,12 @@
 
 void print_alphabet(void)
 {
-	char c = "a";
+	char c = 'a';
 
-	for (c = "a"; c <= "z"; c++)
+	for (c = 'a'; c <= 'z'; c++)
 	{
 		_putchar(c);
 	}
 
-	_putchar("\n");
+	_putchar('\n');
 }
